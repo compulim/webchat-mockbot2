29f4673951d626cbf7b66344ff6f30ccd2e58d42
William Wong <compulim@hotmail.com>
2020-05-24 09:22:22 -0700

    Fix ready

--- src/api/ready/get.js
+++ src/api/ready/get.js
@@ -6,13 +6,13 @@ export default async function getReady(server) {
       const botRes = await fetch(`https://${process.env.WEBSITE_HOSTNAME}/.bot/`);
 
       if (!botRes.ok) {
-        res.send(500, `Direct Line App Service Extension is returning status code ${botRes.status}.`);
+        return res.send(500, `Direct Line App Service Extension is returning status code ${botRes.status}.`);
       }
 
-      const { ib: inbound, ob: outbound } = botRes.json();
+      const { ib: inbound, ob: outbound } = await botRes.json();
 
       if (!inbound || !outbound) {
-        res.send(500, 'Direct Line App Service Extension is not ready.');
+        return res.send(500, 'Direct Line App Service Extension is not ready.');
       }
 
       res.sendRaw('OK', { 'Content-Type': 'text/plain' });
