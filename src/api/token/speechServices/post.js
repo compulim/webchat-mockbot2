53ac24121431ecf6e9a753f6cfbb75a354484cd9
William Wong <compulim@hotmail.com>
2020-05-24 07:57:22 -0700

    Fix speech token

--- src/api/token/speechServices/post.js
+++ src/api/token/speechServices/post.js
@@ -10,18 +10,18 @@ export default async function postTokenSpeechServices(server) {
       return res.send(403, 'Cognitive Services Speech Services authorization token is unavailable.');
     }
 
+    const origin = req.header('origin');
+
+    if (!trustedOrigin(origin)) {
+      return res.send(403, 'not trusted origin', { 'Access-Control-Allow-Origin': '*' });
+    }
+
     console.log(
       `Requesting Speech Services authorization token using subscription key "${SPEECH_SERVICES_SUBSCRIPTION_KEY.substr(
         0,
         3
       )}...${SPEECH_SERVICES_SUBSCRIPTION_KEY.substr(-3)}" for ${origin}`
     );
-
-    const origin = req.header('origin');
-
-    if (!trustedOrigin(origin)) {
-      return res.send(403, 'not trusted origin', { 'Access-Control-Allow-Origin': '*' });
-    }
 
     const tokenRes = await fetch(`https://${SPEECH_SERVICES_REGION}.api.cognitive.microsoft.com/sts/v1.0/issueToken`, {
       headers: { 'Ocp-Apim-Subscription-Key': SPEECH_SERVICES_SUBSCRIPTION_KEY },
